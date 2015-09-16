c294a4059652c8f3340d7f9a8285d9fbe7fdce50
federicofrank <defe.frank@gmail.com>
2015-09-16 11:27:15 -0300

    Configuracion inicial de solucion

---
+++ Src/Alejandria/Alejandria.Win/App_Start/IoCConfig.cs
@@ -0,0 +1,40 @@
+﻿using Framework.Common.Tasks;
+using Framework.Common.Utility;
+using Framework.Ioc;
+using Ninject;
+using Ninject.Extensions.Conventions;
+
+namespace Alejandria.Production.Win
+{
+    public class IoCConfig
+    {
+        public static void Configure(StandardKernel kernel)
+        {
+            kernel.Bind(x => x.FromAssembliesMatching("Alejandria.*")
+                                 .SelectAllClasses()
+                                 .BindAllInterfaces()
+                                 .Configure(c => c.InTransientScope()));
+
+            kernel.Bind(x => x.FromAssembliesMatching("Framework.*")
+                                 .SelectAllClasses()
+                                 .BindAllInterfaces()
+                                 .Configure(c => c.InTransientScope()));
+
+            kernel.Bind(x => x.FromAssembliesMatching("Alejandria.*")
+                                 .SelectAllInterfaces()
+                                 .EndingWith("Factory")
+                                 .BindToFactory()
+                                 .Configure(c => c.InSingletonScope()));
+
+            kernel.Bind(x => x.FromThisAssembly()
+                                 .SelectAllInterfaces()
+                                 .Including<IRunAfterLogin>()
+                                 .BindAllInterfaces()
+                                 .Configure(c => c.InSingletonScope()));
+
+            kernel.Bind<IIocContainer>().To<NinjectIocContainer>().InSingletonScope();
+            kernel.Rebind<IClock>().To<Clock>().InSingletonScope();
+            //kernel.Bind<IMessageBoxDisplayService>().To<MessageBoxDisplayService>().InSingletonScope();
+        }
+    }
+}
---
+++ Src/Alejandria/Framework/Framework.Data/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Framework.Data.Helpers;
+using Framework.Data.Interfaces;
+
+namespace Framework.Data.Repository
+{
+    public interface IRepository<T> where T : class, IEntity
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> whereClause,params Expression<Func<T, object>>[] includes);
+        PagedResultList<T> GetAll(PagingCriteria paging, params Expression<Func<T, object>>[] includes);
+        PagedResultList<T> GetAll(PagingCriteria paging, Expression<Func<T, bool>> whereClause, params Expression<Func<T, object>>[] includes);
+        T Get(Guid id);
+        T Get(Expression<Func<T, bool>> whereClause, params Expression<Func<T, object>>[] includes);
+        void Add(T entity);
+        void Edit(T entity);
+        void Delete(T entity);
+        void Delete(Guid id);
+        bool Commit();
+    }
+}
