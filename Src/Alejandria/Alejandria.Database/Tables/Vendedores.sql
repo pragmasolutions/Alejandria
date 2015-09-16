3812b4f9f32088ff538e4f15c7ad814ce92068f9
lindorsito <santiacevedo@gmail.com>
2015-09-16 18:06:57 -0300

    Update Database
    Add Clientes & Vendedores

---
+++ Src/Alejandria/Alejandria.Database/Tables/Vendedores.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Vendedores](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Abreviatura] [nvarchar](10) NULL,
+	[FechaGeneracion] [datetime] NULL,
+	[OperadorId] [uniqueidentifier] NULL,
+ CONSTRAINT [PK_Vendedores] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
