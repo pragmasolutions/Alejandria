f401a8f375c5614d1f0f8e0019cf46665ed8716a
lindorsito <santiacevedo@gmail.com>
2015-10-10 14:24:43 -0300

    FrmVentas

--- Src/Alejandria/Alejandria.Win/Forms/Ventas/FrmVentas.cs
+++ Src/Alejandria/Alejandria.Win/Forms/Ventas/FrmVentas.cs
@@ -5,15 +5,31 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Alejandria.Data.Interfaces;
+using Alejandria.Entities;
+using Alejandria.Win.Enums;
+using Framework.Common.Utility;
 using Telerik.WinControls;
 
 namespace Alejandria.Win.Forms.Ventas
 {
-    public partial class FrmVentas : Telerik.WinControls.UI.RadForm
+    public partial class FrmVentas : FormBase
     {
-        public FrmVentas()
+        private readonly IClock _clock;
+        private Venta _venta;
+        public FrmVentas(IAlejandriaUow uow,IClock clock)
         {
+            Uow = uow;
+            _clock = clock;
+
             InitializeComponent();
         }
+
+        #region Eventos
+        public event EventHandler<Venta> VentaAgregada;
+        #endregion
+
+        #region Properties
+        #endregion
     }
 }
